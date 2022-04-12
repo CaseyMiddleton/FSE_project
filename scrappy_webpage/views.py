f9c3da3a4013a889444ce14938079b194cc4f2c2
nlaberge <43562244+nlaberge@users.noreply.github.com>
2022-04-12 14:59:00 -0600

    Creating models, and results and results page

--- scrappy_webpage/views.py
+++ scrappy_webpage/views.py
@@ -1,9 +1,36 @@
+from http.client import HTTPResponse
+from urllib import response
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 
-def index(request):
-    #return HttpResponse("Hello, world. You're at the polls index.")
-    #template = loader.get_template('scrappy_webpage/index.html')
-    #return HttpResponse(template.render(request))
-    return render(request, 'scrappy_webpage/index.html') 
+from .forms import NameForm
+from scrappy_webpage.models import Searches
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            print(form.cleaned_data)
+            ins = Searches(your_name = form.cleaned_data['your_name'])
+            ins.save()
+
+            #call twitter api
+
+
+            # redirect to a new URL:
+            return render(request, 'scrappy_webpage/results.html', {'ins': ins,})
+            #return HttpResponseRedirect('/results/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'scrappy_webpage/index.html', {'form': form,})
+
+
+def get_results(request):
+    return render(request, "scrappy_webpage/results.html")     
