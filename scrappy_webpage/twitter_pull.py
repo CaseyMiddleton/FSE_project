1e44928b1fd3e075322d328749399810630c0fb3
caseymid48 <caseymiddleton67@gmail.com>
2022-04-20 16:23:57 -0600

    Updated to show error message when no tweets match search term

--- scrappy_webpage/twitter_pull.py
+++ scrappy_webpage/twitter_pull.py
@@ -50,7 +50,7 @@ def write_query(input1, input2 = None, connector = None):
         elif connector == "OR":
             build_query = "(" + input1 + " " + connector + " " + input2  + ")" + lang_key + rt
         else:
-            # if user inputs first word and connector but no second word, default to first word 
+            # if user inputs first word and connector but no second word, default to first word
             build_query = input1 + lang_key + rt
     else:
         build_query = input1 + lang_key + rt
@@ -69,28 +69,32 @@ def retrieve_json(input1,input2=None,connector=None,next_token = None):
 def tweet_looper(tweets):
     all_tweets = []
     # loop through tweets and extract necessary information
-    for tweet in tweets['data']:
-        # 1. Author ID
-        author_id = tweet['author_id']
-        # 2. Time created
-        created_at = tweet['created_at']
-        # 3. Language
-        lang = tweet['lang']
-        # 4. Tweet metrics
-        retweet_count = tweet['public_metrics']['retweet_count']
-        reply_count = tweet['public_metrics']['reply_count']
-        like_count = tweet['public_metrics']['like_count']
-        quote_count = tweet['public_metrics']['quote_count']
-        tot_count = retweet_count + reply_count + like_count + quote_count
-        # 5. source
-        source = tweet['source']
-        # 6. Tweet text
-        text = tweet['text']
+    try:
+        for tweet in tweets['data']:
+            # 1. Author ID
+            author_id = tweet['author_id']
+            # 2. Time created
+            created_at = tweet['created_at']
+            # 3. Language
+            lang = tweet['lang']
+            # 4. Tweet metrics
+            retweet_count = tweet['public_metrics']['retweet_count']
+            reply_count = tweet['public_metrics']['reply_count']
+            like_count = tweet['public_metrics']['like_count']
+            quote_count = tweet['public_metrics']['quote_count']
+            tot_count = retweet_count + reply_count + like_count + quote_count
+            # 5. source
+            source = tweet['source']
+            # 6. Tweet text
+            text = tweet['text']
 
-        tweet_data = [author_id, created_at, lang, tot_count, text]
-        all_tweets.append(tweet_data)
+            tweet_data = [author_id, created_at, lang, tot_count, text]
+            all_tweets.append(tweet_data)
 
-    return all_tweets
+        return all_tweets
+    except:
+        # if no tweets exist for keywords, return message
+        return [[1, 1, 1, 1, "Sorry, no tweets exist with your search terms! Please try again."]]
 
 def get_min_index(lst):
     minimum = min(lst)
